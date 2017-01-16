a2c305ed56188a3f4dfb268eed360b1208f06ab5
bochan773 <bochan773@SURFACE-BOOK>
2017-01-16 10:30:16 +0900

    Xamarin.Formsパッケージの更新、及びGoogleMap表示対応

--- Spotch/Spotch/Models/Post.cs
+++ Spotch/Spotch/Models/Post.cs
@@ -5,6 +5,7 @@ namespace Spotch
     public class Post
     {
         public string text { get; set; }
+        public double[] position { get; set; }
         public DateTime time { get; set; }
     }
 }
