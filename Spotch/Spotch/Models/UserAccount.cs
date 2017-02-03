b92317f7b570fbb042a9b40b6cfbc1288c2495e4
bochan773 <bochan773@SURFACE-BOOK>
2017-02-03 15:47:58 +0900

    UserAccountクラスの追加

---
+++ Spotch/Spotch/Models/UserAccount.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spotch.Models
+{
+    [JsonObject]
+    class UserAccount
+    {
+        [JsonProperty(PropertyName = "userid")]
+        public long user_id { set; get; }
+        [JsonProperty(PropertyName = "pass")]
+        public string password { set; get; }
+        [JsonProperty(PropertyName = "username")]
+        public string username { set; get; }
+        [JsonProperty(PropertyName = "birthday")]
+        public DateTime birthday { set; get; }
+        [JsonProperty(PropertyName = "email")]
+        public string email { set; get; }
+    }
+}
