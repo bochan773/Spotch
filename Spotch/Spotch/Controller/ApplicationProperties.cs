6c9cbc90f33146bcceec16f98d63fa75ee17d4a6
kbc14a17 <kbc14a17@stu.kawahara.ac.jp>
2017-02-07 15:27:46 +0900

    ユーザー情報を保存する処理の追加

---
+++ Spotch/Spotch/Controller/ApplicationProperties.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Spotch.Controller
+{
+    class ApplicationProperties : ISessionRepository
+    {
+        //データを設定
+        public void SetValue<T>(T value) where T : class
+        {
+            App.Current.Properties[typeof(T).FullName] = value;
+        }
+
+        //データを取得
+        public T GetValue<T>() where T : class
+        {
+            if (!App.Current.Properties.ContainsKey(typeof(T).FullName))
+            {
+                return null;
+            }
+            return App.Current.Properties[typeof(T).FullName] as T;
+        }
+
+        //初期化
+        public bool Initilize()
+        {
+            try
+            {
+                App.Current.Properties.Clear();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        //セッションデータ読み込み
+        public Task<bool> LoadAsync()
+        {
+            // 読み込み不要
+            return Task.FromResult(true);
+        }
+
+        //セッションデータ保存
+        public Task<bool> SaveAsync()
+        {
+            // 保存不要
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ Spotch/Spotch/Controller/ISessionRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotch.Controller
+{
+    public interface ISessionRepository
+    {
+        //データを設定
+        void SetValue<T>(T value) where T : class;
+
+        //データを取得
+        T GetValue<T>() where T : class;
+
+        //初期化
+        bool Initilize();
+
+        //セッションデータ読み込み
+        Task<bool> LoadAsync();
+
+        //セッションデータ保存
+        Task<bool> SaveAsync();
+
+    }
+}
