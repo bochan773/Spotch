99bbd4148dc18be2f9365f2431109170bdab953c
bochan773 <bochan773@SURFACE-BOOK>
2017-02-16 20:24:29 +0900

    LoginModelの作成

---
+++ Spotch/Spotch/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spotch.Models
+{
+    [JsonObject]
+    class LoginModel
+    {
+        [JsonProperty(PropertyName = "password")]
+        public string password { set; get; }
+        [JsonProperty(PropertyName = "email")]
+        public string email { set; get; }
+    }
+}
